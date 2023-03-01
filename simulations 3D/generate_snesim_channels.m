b723a4efa715509a50794b0d03b017ebf51a4452
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-03-01 00:23:48 -0600

    3d

--- simulations 3D/generate_snesim_channels.m
+++ simulations 3D/generate_snesim_channels.m
@@ -6,10 +6,11 @@
 
 % user-defined constants
 dims = [128 128 16];
-num_sim = 500;
+num_sim = 2;
 
 % run snesim
 S = snesim_init;
+S.pdf_target = [0.700 0.300];
 S.nx   = dims(1);
 S.ny   = dims(2);
 S.nz   = dims(3);
@@ -22,5 +23,16 @@ S.nsim = num_sim;
 S = snesim(S);
 
 % Collect and save results
-channel_all = reshape(S.D, [], num_sim);
-csvwrite('channel_all.csv', channel_all);
+channel_3d = reshape(S.D, [], num_sim);
+csvwrite('channel_3d.csv', channel_3d);
+
+%% Temp
+clear;clc
+
+S=snesim_init;
+
+S.nx=128;
+S.ny=128;
+S.nsim = 10;
+S.rseed=1;
+S=snesim(S);
