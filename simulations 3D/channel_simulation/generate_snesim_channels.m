31c166e0b3bb3adf0f63423f32e998624621f6ea
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-03-01 04:28:54 -0600

    fix

--- simulations 3D/channel_simulation/generate_snesim_channels.m
+++ simulations 3D/channel_simulation/generate_snesim_channels.m
@@ -5,24 +5,14 @@
 % nsim = number of realizations to simulate
 
 [nx, ny, nz] = deal(128, 128, 16);
-numsim = 6;
+numsim = 20;
 
 S = sgems_get_par('filtersim_cont');
-%S = sgems_get_par('snesim_std');
-S.ti_file = 'snesim_std.ti';
+S.ti_file = 'filtersim_cont_channels.ti';
 S.dim.nx = nx;
 S.dim.ny = ny;
 S.dim.nz = nz;
 S.XML.parameters.Nb_Realizations.value = numsim;
-S.XML.parameters.Scan_Template = [11,11,11];
-S.XML.parameters.Nb_Multigrids_ADVANCED = 5;
-S.XML.parameters.Data_Weights = [0.4, 0.4, 0.2];
 S = sgems_grid(S);
 
 csvwrite('channels_3d.csv', S.data);
-
-G=cartGrid([nx,ny,nz],[1000,1000,100]); G=computeGeometry(G);
-for i=1:6
-    subplot(2,3,i)
-    plotCellData(G, S.data(:,i)); view(-10,85);
-end
